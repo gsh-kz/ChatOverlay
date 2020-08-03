d86f5c6497028845ca9fb4e22c28baa09621cd0f
kazu <kazu78@gmail.com>
2020-08-03 10:54:01 +0900

    sourceMap config modify

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,73 @@
-const MODE = "development";
-const enabledSourceMap = MODE === "development";
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const webpack = require('webpack');
 
-module.exports = {
-  mode: MODE,
-  entry: {
-    popup: './src/js/popup.js',
-    content: './src/js/content.js',
-    background: './src/js/background.js'
-  },
-  output: {
-    path: __dirname + '/plugin',
-    filename: 'js/[name].js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              url: false,
-              sourceMap: enabledSourceMap,
-              importLoaders: 2
+module.exports = (env, options) => {
+  const enabledSourceMap = options.mode === 'development';
+  return {
+    mode: options.mode,
+    entry: {
+      popup: './src/js/popup.js',
+      content: './src/js/content.js',
+      background: './src/js/background.js'
+    },
+    output: {
+      path: __dirname + '/plugin',
+      filename: 'js/[name].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  '@babel/preset-env'
+                ]
+              }
             }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: enabledSourceMap,
-              plugins: () => [require('autoprefixer')]
+          ]
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                url: false,
+                // sourceMap: enabledSourceMap,
+                importLoaders: 2
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                // sourceMap: enabledSourceMap,
+                plugins: () => [require('autoprefixer')]
+              }
+            },
+            {
+              loader: 'sass-loader',
+              // options: {
+              //   sourceMap: enabledSourceMap
+              // }
             }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: enabledSourceMap
-            }
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].css'
-    }),
-    new webpack.ProvidePlugin({$: 'jquery'})
-  ],
-  devtool: 'source-map'
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].css'
+      }),
+      new webpack.ProvidePlugin({$: 'jquery'})
+    ],
+    devtool: enabledSourceMap === true ? 'source-map' : false
+  }
 };
